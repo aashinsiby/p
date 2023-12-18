53f1e0ed719874f8356e8c8a0fe65eb420ccb6f9
aashinsiby <aashin.csa1923@saintgits.org>
2023-12-18 12:01:00 +0530

    1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ObjectComponent } from './object/object.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,ObjectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
